db71ecaf728958a2a664984f64ee28ba39f1eb14
urbanescavenger <32826223+urbanescavenger@users.noreply.github.com>
2025-01-24 09:58:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,4 +25,3 @@ rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 ./scripts/feeds install -a 
-make menuconfig
