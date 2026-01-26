fe8b19e0f7d4af787cb9315539716cdbb75de258
urbanescavenger <32826223+urbanescavenger@users.noreply.github.com>
2026-01-26 15:25:29 +0800

    Update diy-part2.sh
    测试修改

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-sed -i 's/192.168.1.1/192.168.31.160/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.160/g'  package/base-files/luci/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
